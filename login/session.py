12f13727026c21f9e1befdc70846d43dc7cfd807
munimai0909 <munimai.acc@gmail.com>
2025-04-18 22:56:30 +0530

    Update session.py

--- login/session.py
+++ login/session.py
@@ -1,1 +1,11 @@
+session_store = {}
 
+def set_user_session(session_id, username):
+    session_store[session_id] = username
+
+def get_user_from_session(session_id):
+    return session_store.get(session_id)
+
+def clear_session(session_id):
+    if session_id in session_store:
+        del session_store[session_id]
