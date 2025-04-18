14dc2e5a32ce94cbedf3f284e5c9bb15ee80dca6
munimai0909 <munimai.acc@gmail.com>
2025-04-18 22:56:01 +0530

    Update auth.py

--- login/auth.py
+++ login/auth.py
@@ -1,1 +1,13 @@
+users = {}
+
+def register_user(username, password):
+    if username in users:
+        return False
+    users[username] = {"password": password}
+    return True
+
+def login_user(username, password):
+    if username in users and users[username]["password"] == password:
+        return True
+    return False
 
